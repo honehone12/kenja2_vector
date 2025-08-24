6fa6e108d054e0c21f791426fa3fc174e35c0c0e
Twelfthonehone <honehone.41games@gmail.com>
2025-08-24 16:58:48 +0900

    removed text field from document

--- documents/documents.py
+++ documents/documents.py
@@ -1,10 +1,8 @@
 from typing import TypedDict
 from bson import ObjectId
 
-TXT_VEC_FIELD = 'text_vector'
 IMG_VEC_FIELD = 'image_vector'
 
 class FlatDoc(TypedDict):
     _id: ObjectId
     img: str
-    description: str | None
